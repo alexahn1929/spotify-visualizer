82f068dad4b21e1696da4b0da27ce72706405de9
alexahn1929 <121832249+alexahn1929@users.noreply.github.com>
2023-08-18 17:54:11 -0400

    Set up Flask backend
    Connects Spotify API functions to Flask backend API, with the endpoint
    /api/playlist/ accepting a Spotify playlist ID at the end of its path
    and returning the associated playlist JSON with PCA-reduced
    coordinates.

--- api.py
+++ api.py
@@ -1,13 +1,14 @@
 import requests
 from dotenv import load_dotenv
 import os
-from urllib.parse import urlparse
 import pandas as pd
 import numpy as np
-import json
-import pickle
 import matplotlib.pyplot as plt
-import tabloo
+from flask import Flask, Response, abort
+from markupsafe import escape
+# import pickle
+# import tabloo
+# from urllib.parse import urlparse
 
 load_dotenv()
 
@@ -23,17 +24,18 @@ def getToken():
     return output["access_token"]
 # print("Sample token:", getToken())
 
-def getPlaylistAPIUrl(rawURL):
-    HOSTNAME = "open.spotify.com"
-    PATH_PREFIX = "/playlist/"
+# def getPlaylistAPIUrl(rawURL):
+#     HOSTNAME = "open.spotify.com"
+#     PATH_PREFIX = "/playlist/"
 
-    playlistURL = urlparse(rawURL)
+#     playlistURL = urlparse(rawURL)
 
-    if playlistURL.hostname != HOSTNAME or PATH_PREFIX not in playlistURL.path:
-        raise Exception("Could not parse playlist ID from URL")
+#     if playlistURL.hostname != HOSTNAME or PATH_PREFIX not in playlistURL.path:
+#         raise Exception("Could not parse playlist ID from URL")
     
-    playlistID = playlistURL.path[playlistURL.path.rindex("/")+1:]
-    return f"https://api.spotify.com/v1/playlists/{playlistID}/tracks"
+#     playlistID = playlistURL.path[playlistURL.path.rindex("/")+1:]
+#     return f"https://api.spotify.com/v1/playlists/{playlistID}/tracks"
+
 # print("Sample playlist API URL:", getPlaylistAPIUrl("https://open.spotify.com/playlist/3AFvuS9t4qLhaaLBHRcSqk?si=de0c34a3ac7549bf"))
 
 def makeAPIRequest(apiURL):
@@ -44,6 +46,8 @@ def makeAPIRequest(apiURL):
         return makeAPIRequest(apiURL)
     elif response.status_code == 403:
         raise Exception("Bad OAuth request")
+    elif response.status_code == 404:
+        raise Exception("Invalid playlist ID")
     elif response.status_code == 429:
         raise Exception("Spotify API rate limit exceeded")
     return response.json()
@@ -52,63 +56,36 @@ def getAudioFeatures(songs): #can take 100 songs max, returns array of song audi
     songIdsCommaSeparated = ",".join([song["id"] for song in songs])
     return makeAPIRequest(f"https://api.spotify.com/v1/audio-features?ids={songIdsCommaSeparated}")["audio_features"]
 
-
-def getPlaylistRaw(rawURL):
-    playlistJSON = makeAPIRequest(getPlaylistAPIUrl(rawURL))
-    songs = [song["track"] for song in playlistJSON["items"]]
+def getPlaylist(playlistID):
+    playlistJSON = makeAPIRequest(f"https://api.spotify.com/v1/playlists/{playlistID}/tracks")
+    songs = [song["track"] for song in playlistJSON["items"] if song["track"]["id"]] # local files will not have a song id and must be removed
     for i, features in enumerate(getAudioFeatures(songs)):
         songs[i]["audio_features"] = features
     while playlistJSON["next"]:
         playlistJSON = makeAPIRequest(playlistJSON["next"])
-        songsToAppend = [song["track"] for song in playlistJSON["items"]]
+        songsToAppend = [song["track"] for song in playlistJSON["items"] if song["track"]["id"]]
         for i, features in enumerate(getAudioFeatures(songsToAppend)):
             songsToAppend[i]["audio_features"] = features
         songs += songsToAppend
     return songs
-# sampleSongs = getPlaylistRaw("https://open.spotify.com/playlist/1ID56tk92tPTeIJ5jH8aUb?si=571fdb006e8244a2")
+# sampleSongs = getPlaylist("https://open.spotify.com/playlist/1ID56tk92tPTeIJ5jH8aUb?si=571fdb006e8244a2")
 # with open('summer22.obj', 'wb') as fileObj:
 #     pickle.dump(sampleSongs, fileObj)
 # print("Sample song structure:", sampleSongs[0], len(sampleSongs))
 # print()
 # print(sampleSongs[-1])
 
+app = Flask(__name__)
 
-
-'''
-Generates a pd.DataFrame representing the tracks in the playlist and their IDs, href, names,
-album (objects), artists, and duration.
-
-:returns: a pandas DataFrame containing relevant track information in a given playlist
-'''
-def generate_playlist_df(rawURL):
-    #script for manipulating track data into a usable format, stored in a provisional dataframe
-    track_list = getPlaylistRaw(rawURL)
-    if not track_list:
-        #raise error, empty playlist
-        pass
-    playlist_df = pd.DataFrame()
-    for t in track_list:
-        track_info = t['track']
-        album = track_info['album']
-        href = track_info['href']
-        t_id = track_info['id']
-        name = track_info['name']
-        artists = track_info['artists']
-        duration = track_info['duration_ms']
-        temp_arr = [t_id, href, name, album, artists, duration]
-
-        temp_df = pd.DataFrame(data = temp_arr)
-        playlist_df = pd.concat([playlist_df, temp_df], axis = 1)
-    playlist_df = playlist_df.T
-    playlist_df = playlist_df.rename(columns = {0: 'ID', 1:'href', 2:'Name', 3:'Album', 4:'Artists', 5:'Duration'}).reset_index()
-    playlist_df = playlist_df.drop(columns = ['index'])
-    return playlist_df
-# sample_df = generate_playlist_df("https://open.spotify.com/playlist/3AFvuS9t4qLhaaLBHRcSqk?si=0767d59f3bb9499c")
-# print(sample_df)
-
-
-def getDf(songs):
+@app.route("/api/playlist/<playlistID>")
+def getDf(playlistID):
+    try:
+        songs = getPlaylist(escape(playlistID))
+    except Exception as e:
+        abort(404, e)
+    
     df = pd.DataFrame(songs)
+    df = df[df["name"].ne("")] # old songs that have been deleted from spotify still have some data associated with them - this is a makeshift fix
     df["album_image"] = df["album"].apply(lambda x: x["images"][0]["url"]) # could use [-1] to save bandwidth
     df["album"] = df["album"].apply(lambda x: x["name"])
     df["artists"] = df["artists"].apply(lambda x: ", ".join([artist["name"] for artist in x]))
@@ -129,8 +106,7 @@ def getDf(songs):
     k = 3 # select the number of principal components
     reduced_data = pd.DataFrame(np.matmul(data, sorted_eigenvectors[:,:k]), columns=["x", "y", "z"]) # transform the original data
     processed_data = pd.concat([metadata, reduced_data], axis=1)
-    processed_data.to_json("out.json", orient="index")
-
+    
     # explained_variance = sorted_eigenvalues / np.sum(sorted_eigenvalues)
     # cumulative_variance = np.cumsum(explained_variance)
 
@@ -145,10 +121,9 @@ def getDf(songs):
     # plt.ylabel('Proportion of variance')
     # plt.show()
     
-    # print(metadata["preview_url"].isna().value_counts())
-    return processed_data
+    return Response(processed_data.to_json(orient="index"), mimetype="application/json")
 
-fileObj = open('summer22.obj', 'rb')
-songsSaved = pickle.load(fileObj)
-fileObj.close()
-getDf(songsSaved)
+# fileObj = open('summer22.obj', 'rb')
+# songsSaved = pickle.load(fileObj)
+# fileObj.close()
+# getDf(songsSaved)
